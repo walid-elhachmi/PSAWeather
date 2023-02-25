9da82c2aec142fd02a39cbdf920fb7c8bfae8c9c
Walid EL HACHMI <walid.elhachmi@trutify.com>
2023-02-25 23:27:21 +0100

    Initial Commit

---
+++ PSAWeather.UI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PSAWeather.UI
+//
+//  Created by Walid El Hachmi on 25/2/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
