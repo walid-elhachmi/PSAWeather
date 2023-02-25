3a9aa681fd2ab99767a7a5d39f56e2897d2bc588
Walid EL HACHMI <walid.elhachmi@trutify.com>
2023-02-26 00:11:02 +0100

    Create a networking layer as framework

---
+++ PSAWeather.Remote/PSAWeather_Remote.h
@@ -0,0 +1,18 @@
+//
+//  PSAWeather_Remote.h
+//  PSAWeather.Remote
+//
+//  Created by Walid El Hachmi on 25/2/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PSAWeather_Remote.
+FOUNDATION_EXPORT double PSAWeather_RemoteVersionNumber;
+
+//! Project version string for PSAWeather_Remote.
+FOUNDATION_EXPORT const unsigned char PSAWeather_RemoteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PSAWeather_Remote/PublicHeader.h>
+
+
