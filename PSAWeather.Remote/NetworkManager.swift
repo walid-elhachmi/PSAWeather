7e93181df100440f20ea31ed56e18cdf8bd7c580
Walid EL HACHMI <walid.elhachmi@trutify.com>
2023-02-26 09:47:13 +0100

    Create a network manager to handle request and get data from the API

---
+++ PSAWeather.Remote/NetworkManager.swift
@@ -0,0 +1,52 @@
+//
+//  NetworkManager.swift
+//  PSAWeather.Remote
+//
+//  Created by Walid El Hachmi on 26/2/2023.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case invalidResponse
+    case emptyData
+    case decodingError(Error)
+}
+
+public class NetworkManager {
+    private let urlSession: URLSession
+    
+    public init() {
+        let configuration = URLSessionConfiguration.default
+        self.urlSession = URLSession(configuration: configuration)
+    }
+    
+    public func request<T: Codable>(url: URL, completion: @escaping (Result<T, Error>) -> Void) {
+        let task = urlSession.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                    completion(.failure(NetworkError.invalidResponse))
+                    return
+                }
+                
+                
+                guard let data = data, !data.isEmpty else {
+                    completion(.failure(NetworkError.emptyData))
+                    return
+                }
+                
+                do {
+                    let decoder = JSONDecoder()
+                    let decodeData = try decoder.decode(T.self, from: data)
+                    completion(.success(decodeData))
+                } catch {
+                    completion(.failure(NetworkError.decodingError(error)))
+                }
+            }
+        }
+        
+        task.resume()
+    }
+}
