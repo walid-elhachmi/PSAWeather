6bcd7b8a99daca51130391ab307c6fc0b336b7e6
Walid EL HACHMI <walid.elhachmi@trutify.com>
2023-02-26 21:49:05 +0100

    Display an empty label to indicate what should be done if the list is empty

--- PSAWeather.UI/Screens/CitiesList/Views/CitiesViewController.swift
+++ PSAWeather.UI/Screens/CitiesList/Views/CitiesViewController.swift
@@ -13,30 +13,49 @@ class CitiesViewController: UIViewController {
     private var cityViewModel = CityViewModel()
     private let dataSource = CitiesDataSource()
     
+    private var isEmptyData: Bool = false {
+        didSet {
+            if isEmptyData {
+                tableView.isHidden = true
+                emptyLabel.isHidden = false
+            } else {
+                tableView.isHidden = false
+                emptyLabel.isHidden = true
+            }
+        }
+    }
+    
     //@IBOutlets
     @IBOutlet private weak var tableView: UITableView!
+    @IBOutlet private weak var emptyLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addCityTapped))
         
-        dataSource.data = cityViewModel.getCities()
-        dataSource.didSelect = { [weak self] cityName in
-            let weatherDetailViewController = WeatherDetailViewController.instantiateFromStoryboard(named: "Main")
-            weatherDetailViewController.cityName = cityName
-            self?.navigationController?.pushViewController(weatherDetailViewController, animated: true)
-        }
+        configureDataSource()
         
         tableView.dataSource = dataSource
         tableView.delegate = dataSource
         tableView.register(cellClass: CityViewCell.self)
+        
+        isEmptyData = dataSource.data.isEmpty
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         self.navigationController?.navigationBar.tintColor = .black
+    }
+    
+    private func configureDataSource() {
+        dataSource.data = cityViewModel.getCities()
+        dataSource.didSelect = { [weak self] cityName in
+            let weatherDetailViewController = WeatherDetailViewController.instantiateFromStoryboard(named: "Main")
+            weatherDetailViewController.cityName = cityName
+            self?.navigationController?.pushViewController(weatherDetailViewController, animated: true)
+        }
     }
     
     @objc
@@ -50,6 +69,7 @@ class CitiesViewController: UIViewController {
             self?.dataSource.data.append(CityModel(name: name))
             self?.cityViewModel.addCity(name: name)
             self?.tableView.reloadData()
+            self?.isEmptyData = false
         }
         
         let cancelAction = UIAlertAction(title: "Cancel",  style: .cancel)
